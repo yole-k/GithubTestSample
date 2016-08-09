ccf31c2fccfe265bf3b91347294aadaf898a70d5
yole-k <yole.karif@gmail.com>
2016-08-09 22:05:05 +0900

    First Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Gtk;
+
+namespace GithubTestSample
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Application.Init();
+			MainWindow win = new MainWindow();
+			//"test";
+			win.Show();
+			Application.Run();
+		}
+	}
+}
